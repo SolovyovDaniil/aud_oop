1b011eed4abc406f688db6f9c576cbb5bf715fc8
erux_ <erux_@LAPTOP-AL4582O6>
2024-04-01 17:48:28 +0300

    1

--- aud_oop/Form1.Designer.cs
+++ aud_oop/Form1.Designer.cs
@@ -57,7 +57,6 @@
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridView1.Location = new Point(25, 28);
             dataGridView1.Name = "dataGridView1";
-            dataGridView1.ReadOnly = true;
             dataGridView1.RowHeadersWidth = 62;
             dataGridView1.RowTemplate.Height = 33;
             dataGridView1.Size = new Size(1121, 618);
